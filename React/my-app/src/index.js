191b767cfbe2fee24cb58af3983ff0aae90e0fc9
Rushv10 <rushivadanerv18@gmail.com>
2025-01-29 22:13:51 +0530

    Updated dependencies and added Vercel config

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NewsletterFooter from "./NewsletterFooter";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsletterFooter from "./NewsletterFooter";
 import Home from "./Home";
 import NavHeader from "./NavHeader"; // Import your DropdownMenu component
 import Userlogin from "./Userlogin"; // Import your Userlogin component
@@ -13,20 +13,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* Router wraps all the routes */}
     <Router>
-      {/* Include the navigation header across all pages */}
-      <NavHeader />
-
-      {/* Define Routes */}
+      <NavHeader /> {/* Header appears on all pages */}
       <Routes>
+        <Route path="/" element={<Home />} /> {/* Default Route */}
         <Route path="/userlogin" element={<Userlogin />} />
         <Route path="/accountopen" element={<AccountOpen />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/ChatboxSupport" element={<ChatboxSupport />} />
-        <Route path="/CustomerLogin" element={<CustomerLogin />} />
+        <Route path="/chatboxsupport" element={<ChatboxSupport />} />
+        <Route path="/customerlogin" element={<CustomerLogin />} />
       </Routes>
+      <NewsletterFooter /> {/* Footer appears on all pages */}
     </Router>
-    <NewsletterFooter />
   </React.StrictMode>
 );
